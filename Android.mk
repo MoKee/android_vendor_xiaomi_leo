ae258cccd7c240ce18643ba4282c18aa0d6371fb
nian0114 <268078545@qq.com>
2016-06-25 11:49:56 +0800

    Update vendor
    Change-Id: Id104b22e8419404e365bef7a40f15c25d4fe220d

--- Android.mk
+++ Android.mk
@@ -129,3 +129,4 @@ $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/sy
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
 endif
+
